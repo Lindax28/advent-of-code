0345c779c7b7fbb3f40af83bcde40a61a4f0be7b
Linda Xiao <Lindax28@hotmail.com>
2021-10-12 12:43:04 -0700

    Complete 2020 - Day 1

---
+++ 2020/1.go
@@ -0,0 +1,78 @@
+/* --- Day 1: Report Repair ---
+After saving Christmas five years in a row, you've decided to take a vacation at a nice resort on a tropical island. Surely, Christmas will go on without you.
+
+The tropical island has its own currency and is entirely cash-only. The gold coins used there have a little picture of a starfish; the locals just call them stars. None of the currency exchanges seem to have heard of them, but somehow, you'll need to find fifty of these coins by the time you arrive so you can pay the deposit on your room.
+
+To save your vacation, you need to get all fifty stars by December 25th.
+
+Collect stars by solving puzzles. Two puzzles will be made available on each day in the Advent calendar; the second puzzle is unlocked when you complete the first. Each puzzle grants one star. Good luck!
+
+Before you leave, the Elves in accounting just need you to fix your expense report (your puzzle input); apparently, something isn't quite adding up.
+
+Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+
+For example, suppose your expense report contained the following:
+
+1721
+979
+366
+299
+675
+1456
+In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+
+Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+*/
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+// `readLines` reads a file into memory and returns a slice of its lines
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+// `contains` returns whether a string array contains a given string
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	lines, err := readLines("1_data.txt")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+	for _, year := range lines {
+		num, err := strconv.Atoi(year)
+		if err != nil {
+			log.Fatalf("strconv: %s", err)
+		}
+		if contains(lines, strconv.Itoa(2020-num)) {
+			fmt.Println(num * (2020 - num))
+			break
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code
+
+Puzzles solved using Golang.
