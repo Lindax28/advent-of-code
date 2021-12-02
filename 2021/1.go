7254b453c32a9af171074f0ee98752c8f86e7bf1
lindaxia <lindaxia@twitch.tv>
2021-12-01 23:07:38 -0800

    Add Part 2 instructions

--- 2021/1.go
+++ 2021/1.go
@@ -40,6 +40,39 @@ To do this, count the number of times a depth measurement increases from the pre
 In this example, there are 7 measurements that are larger than the previous measurement.
 
 How many measurements are larger than the previous measurement?
+
+--- Part Two ---
+Considering every single measurement isn't as useful as you expected: there's just too much noise in the data.
+
+Instead, consider sums of a three-measurement sliding window. Again considering the above example:
+
+199  A
+200  A B
+208  A B C
+210    B C D
+200  E   C D
+207  E F   D
+240  E F G
+269    F G H
+260      G H
+263        H
+Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+
+In the above example, the sum of each three-measurement window is as follows:
+
+A: 607 (N/A - no previous sum)
+B: 618 (increased)
+C: 618 (no change)
+D: 617 (decreased)
+E: 647 (increased)
+F: 716 (increased)
+G: 769 (increased)
+H: 792 (increased)
+In this example, there are 5 sums that are larger than the previous sum.
+
+Consider sums of a three-measurement sliding window. How many sums are larger than the previous sum?
 */
 
 package main
@@ -64,31 +97,31 @@ func main() {
 	countSum := 0
 
 	scanner.Scan()
-	window1, _ := strconv.Atoi(scanner.Text())
+	pane1, _ := strconv.Atoi(scanner.Text())
 	scanner.Scan()
-	window2, _ := strconv.Atoi(scanner.Text())
+	pane2, _ := strconv.Atoi(scanner.Text())
 	scanner.Scan()
-	window3, _ := strconv.Atoi(scanner.Text())
+	pane3, _ := strconv.Atoi(scanner.Text())
 
-	if window2 > window1 {
+	if pane2 > pane1 {
 		countSingle += 1
 	}
-	if window3 > window2 {
+	if pane3 > pane2 {
 		countSingle += 1
 	}
 
 	for scanner.Scan() {
 		depth, _ := strconv.Atoi(scanner.Text())
-		if depth > window3 {
+		if depth > pane3 {
 			countSingle += 1
 		}
-		if depth > window1 {
+		if depth > pane1 {
 			countSum += 1
 		}
-		window1 = window2
-		window2 = window3
-		window3 = depth
+		pane1 = pane2
+		pane2 = pane3
+		pane3 = depth
 	}
 	fmt.Println("# of depth increases:", countSingle)
-	fmt.Println("# of depth-window increases:", countSum)
+	fmt.Println("# of window increases:", countSum)
 }
